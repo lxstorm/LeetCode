82136e78b8bb06c72a3f06efd11dc3ac03e37e36
lx <lxstormy@gmail.com>
2016-03-24 00:21:05 +0800

    zigzag

---
+++ src/ZigZagConversion.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string
+        int numCols = (s.size() + numRows - 1)/ numRows;
+        char a[numRows][numCols];
+        int i = 0;
+        while(i < s.size()){
+            if(col % 2 == 0){
+
+            }
+        }
+    }
+};
