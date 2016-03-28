50d0bd44394458353f9e35f5d26cb57278043b8c
lx <lxstormy@gmail.com>
2016-03-28 23:01:38 +0800

    StringtoInteger

--- src/StringtoInteger.cpp
+++ src/StringtoInteger.cpp
@@ -4,21 +4,38 @@ public:
         long long res = 0;
         int sgn = 1;
         int len = str.size();
-        for(int i = 0;i != len;++i){
-            if(i == 0){
-                if(str[0] == '-')
-                    sgn = -1;
-                if(str[0] ==  '+')
-                ;
-            }
-            else{
-                res = res * 10 + (str[i] - '0');
-                cout<<res<<endl;
-            }
+        int i = 0;
+        int start = 0;
+
+        while(str[i] == ' ')
+            ++i;
+        start = i;
+        if(str[i] == '-'){
+            sgn = -1;
+            ++i;
         }
+        else if(str[i] == '+'){
+            ++i;
+        }
+        while(i != len){
+            if('0' <= str[i] && str[i] <= '9')
+                res = res * 10 + (str[i] - '0');
+            else {
+                if(i - start == 1)
+                    if(str[start] == '+' || str[start] == '-')
+                        return 0;
+                break;
+                }
+            ++i;
+            if (res > INT_MAX)
+                break;
+        }
+
         res = res * sgn;
-        if (res > INT_MAX || res <INT_MIN)
-            return 0;
+        if (res > INT_MAX)
+            return INT_MAX;
+        if (res < INT_MIN)
+            return INT_MIN;
         return (int)res;
     }
 };
