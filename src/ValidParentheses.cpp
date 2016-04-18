47678e575b967cfdd335a1f9658abf8b169b85f4
lxstorm <lxstormy@gmail.com>
2016-04-18 20:10:13 +0800

    ValidParentheses

---
+++ src/ValidParentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> mystack;
+        char tmpIn;
+        char tmpOut;
+        for(int i = 0;i < s.size();++i){
+            tmpIn = s[i];
+            if(tmpIn == '(' || tmpIn == '[' || tmpIn == '{')
+                mystack.push(s[i]);
+            if(tmpIn == ')' || tmpIn == ']' || tmpIn == '}'){
+                if(mystack.empty())
+                    return false;
+                tmpOut = mystack.top();
+                if((tmpIn == ')' && tmpOut != '(')
+                || (tmpIn == ']' && tmpOut != '[')
+                || (tmpIn == '}' && tmpOut != '{'))
+                    return false;
+                mystack.pop();
+
+            }
+        }
+        if(mystack.empty())
+            return true;
+        else
+            return false;
+    }
+};
+
+    a try to use stack and map
+class Solution{
+public:
+    bool isValid(string s){
+        stack<char> st;
+        map<char,char> m={{')', '('},{']', '['},{'}', '{'}};
+        for(int i = 0;i < s.size();++i){
+            tmp = s[i];
+            if(tmp == '(' || tmp == '[' || tmp == '{')
+                st.push(tmp);
+            if(tmp == ')' || tmp == ']' || tmp == '}'){
+                if(st.empty() || st.top() != m[tmp])
+                    return false;
+                st.pop();
+            }
+            return st.empty();
+        }
+    }
+}
