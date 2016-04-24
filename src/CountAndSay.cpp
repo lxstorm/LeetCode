7d96b0fe062498cc2d30afe9a10ffb24669939c3
lx <lxstormy@gmail.com>
2016-04-24 21:58:10 +0800

    Count and Say

--- src/CountAndSay.cpp
+++ src/CountAndSay.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        if(n == 0)
+            return "";
 
+        string res = "1";
+        string nres;
+        while(--n){
+            nres = "";
+            for(int i = 0; i < res.size();++i){
+                int cnt = 1;
+                while(i < res.size() - 1 && (res[i] == res[i + 1])){
+                    ++cnt;
+                    ++i;
+                }
+            nres += cnt + '0';
+            nres += res[i];
+            }
+            res = nres;
+        }
+        return res;
     }
 };
