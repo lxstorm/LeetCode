a12713a9f93a5a13698cd5e4c9ba67a655bcc580
lx <lxstormy@gmail.com>
2016-04-26 23:56:39 +0800

    3Sum

---
+++ src/3Sum.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int len = nums.size();
+        for(int i = 0;i < len;++i){
+            unordered_map<>
+        }
+    }
+};
