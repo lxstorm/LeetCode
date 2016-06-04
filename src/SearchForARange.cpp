930d627d59eff9693c0c6d13cc57ee03e588abca
lx <lxstormy@gmail.com>
2016-06-04 23:03:09 +0800

    Search For A Range

--- src/SearchForARange.cpp
+++ src/SearchForARange.cpp
@@ -51,6 +51,7 @@ public:
     }
 };
 */
+//it's a divide and conquer solution
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
@@ -59,18 +60,22 @@ public:
 
         return helper(nums, left, right, target);
     }
+private:
     vector<int> helper(vector<int>& nums, int left, int right, int target){
+        // it's the final condition
         if(nums[left] == target && nums[right] == target)
             return {left, right};
         if(nums[left] <= target && target <= nums[right]){
             int mid = (left + right) / 2;
-            vector<int> l = helper(nums, l, mid, target);
+            // get two part range, l is the left range, l[0] is the target start
+            // point, r[1] is the target end point.
+            vector<int> l = helper(nums, left, mid, target);
             vector<int> r = helper(nums, mid + 1, right, target);
             if(l[0] == -1)
-                return right;
-            if(right[1] == -1)
-                return left;
-            return {left[0], right[1]};
+                return r;
+            if(r[1] == -1)
+                return l;
+            return {l[0], r[1]};
         }
         return {-1, -1};
     }
@@ -78,13 +83,17 @@ public:
 /******************************************************************************/
 /*    two binary search methond
  *    two mid and its explanation
- *    the first mid = (i + j) / 2; consider that if the sequence is {0,0,0,1,1}
  *    1.it is left biased, the mid is always divided into left section;
- *        if there left two element 0 1, then we can have
- *        [0 0] [1 1] part(we want to get 1, now the mid is 0)
- *    2.the second mid = (i + j) / 2 + 1;
- *      the mid is always divided into the right section;
-
+ *    2.the second mid = (i + j) / 2 + 1，the mid is always divided into the right section;
+ *    we do this because if the sequence is 0 0 0 0 1 1 1 1 1 1 2 2 2
+ *    there is no difference in both case if the mid is in the 0's sequence
+ *    or in the 2's sequence,
+ *    when we are in the 1's sequence, in the first case, we divide know we
+ *    should search the left part, when come to 0 0 0 0 1 1 ... we need to let
+                                                        ^
+ *    the first 1 include in the left part, so we should have (i, mid) (mid + 1, j)
+ *    in the second case we should have (i, mid - 1) (mid, j)
+ *    in order to have mid - 1 not less than i, we let mid = (i + j) / 2 + 1;
 class Solution{
 public:
     vector<int> searchRange(vector<int>& nums, int target){
