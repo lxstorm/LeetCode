7ee92712e452f48de5e055c92183d8ee626107d3
lxstorm <lxstormy@gmail.com>
2016-09-13 19:22:22 +0800

    CombinationSum

--- src/CombinationSum.cpp
+++ src/CombinationSum.cpp
@@ -12,6 +12,11 @@ public:
 private:
     void elementSum(vector<int>& candidates, vector<int>& combination,
             vector<vector<int>>& result,int begin, int target){
+        // see how combination change
+        // for(int i = 0;i<combination.size();++i){
+        //     cout<<combination[i]<<" ";
+        // }
+        cout<<endl;
         if(target == 0){
             result.push_back(combination);
             return;
@@ -26,3 +31,21 @@ private:
         }
     }
 };
+/* if candidates is [10,11,12] target is 33
+ * combination:
+    10
+    10 10
+    10 10 10
+    10 10 11
+    10 10 12
+    10 11
+    10 11 11
+    10 11 12
+    10 12
+    11
+    11 11
+    11 11 11
+    11 12
+    12
+    12 12
+ */
---
+++ src/CombinationSumII.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
+        
+    }
+};
