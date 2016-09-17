3504ace9069762a37a1d4e855cdd8c1b05bef66c
lx <lxstormy@gmail.com>
2016-09-17 14:39:21 +0800

    Maximum Subarray

---
+++ src/MaximumSubarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+
+        int sum = nums[0];
+        int max = nums[0];
+
+        for(int i = 1;i < nums.size();++i){
+            if(sum >= 0){
+                sum += nums[i];
+            }
+            else{
+                sum = nums[i];
+            }
+            if(sum > max)
+                max = sum;
+        }
+        return max;
+    }
+};
