02e362413bc7ddbe966b86fb40fe214f924c1f71
lx <lxstormy@gmail.com>
2016-10-05 19:37:38 +0800

    Remove Duplicates from Sorted Array II

---
+++ src/RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int j = 0;j < nums.size();++j){
+            // it's i - 2 because i is point to the index to be inserted.
+            // we only need the new insert number not equal to the i - 2
+            if(j < 2 || nums[j] != nums[i - 2])
+                nums[i++] = nums[j];
+        }
+        return i;
+    }
+};
