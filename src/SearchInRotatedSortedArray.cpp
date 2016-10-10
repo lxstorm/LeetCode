3bc1e17c49c740ce722f0f7e40fd87830de49905
lx <lxstormy@gmail.com>
2016-10-10 17:18:11 +0800

    some fix

--- src/SearchInRotatedSortedArray.cpp
+++ src/SearchInRotatedSortedArray.cpp
@@ -21,3 +21,28 @@ public:
         return nums[l] == target ? l : -1;
     }
 };
+/* another one */
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] >= nums[l]){
+                if((target >= nums[l]) && (target < nums[mid]))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else{
+                if((target <= nums[r]) && (target > nums[mid]))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return nums[l] == target ? l : -1;
+
+    }
+};
